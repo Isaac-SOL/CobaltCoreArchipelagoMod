caf51e75d193ce648ff32accfcbcbae3f6b14170
Pierre Cuquel <p.cuquel@free.fr>
2026-01-17 20:04:43 +0100

    NewRunOptions minor tweaks

--- MenuPatches/NewRunOptionsPatches.cs
+++ MenuPatches/NewRunOptionsPatches.cs
@@ -4,14 +4,33 @@ namespace CobaltCoreArchipelago.MenuPatches;
 
 public class NewRunOptionsPatches;
 
-
-
-// Remove dailies button, not supported
+// No, dailies are never unlocked because we don't support them
 [HarmonyPatch(typeof(NewRunOptions), nameof(NewRunOptions.AreDailiesUnlocked))]
 public class DailiesPatch
 {
     public static void Postfix(ref bool __result)
     {
         __result = false;
+    }
+}
+
+// Actually just prevent dailies button from being drawn at all
+[HarmonyPriority(Priority.High)]
+[HarmonyPatch(typeof(SharedArt), nameof(SharedArt.ButtonText))]
+public class ButtonTextPatch
+{
+    public static bool Prefix(UIKey key)
+    {
+        return key.k != StableUK.newRun_dailyRun;
+    }
+}
+
+// Unhide Books and CAT
+[HarmonyPatch(typeof(Character), nameof(Character.Render))]
+public class CharacterRenderPatch
+{
+    public static void Prefix(ref bool hideFace)
+    {
+        hideFace = false;
     }
 }
