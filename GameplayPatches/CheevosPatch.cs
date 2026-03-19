9b77b21b932987cc738d9fc09763460aeea84032
Pierre Cuquel <p.cuquel@free.fr>
2026-03-19 16:16:19 +0100

    Do not get vanilla achievements

---
+++ GameplayPatches/CheevosPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Microsoft.Extensions.Logging;
+
+namespace CobaltCoreArchipelago.GameplayPatches;
+
+// Do not give achievements
+[HarmonyPatch(typeof(Cheevos), nameof(Cheevos.ShouldDoCheevos))]
+[HarmonyPriority(Priority.Low)]
+public class CheevosPatch
+{
+    static void Postfix(ref bool __result)
+    {
+        if (__result) ModEntry.Instance.Logger.LogDebug("Prevented getting an achievement.");
+        __result = false;
+    }
+}
