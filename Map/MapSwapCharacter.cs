16d3c4349de13f5f1ef115330eb95cfb3433dcbf
Pierre Cuquel <p.cuquel@free.fr>
2026-04-06 21:48:37 +0200

    Added node spawn logic to map

---
+++ Map/MapSwapCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CobaltCoreArchipelago.Actions;
+
+namespace CobaltCoreArchipelago.Map;
+
+public class MapSwapCharacter : MapNodeContents
+{
+    public override void Render(G g, Vec v)
+    {
+        Draw.Sprite(AArchipelagoCheckLocation.Spr, v.x, v.y);
+    }
+
+    public override List<Tooltip> GetTooltips(G g)
+    {
+        return [new TTText("Faut swap des persos la")];
+    }
+
+    public override Route MakeRoute(State s, Vec coord)
+    {
+        throw new System.NotImplementedException();
+    }
+}
