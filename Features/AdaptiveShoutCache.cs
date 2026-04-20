9899fec3f365e433d74275126f4dfe4af6ecf907
Pierre Cuquel <p.cuquel@free.fr>
2026-04-20 14:55:53 +0200

    Added support for "once" compshouts

--- Features/AdaptiveShoutCache.cs
+++ Features/AdaptiveShoutCache.cs
@@ -11,18 +11,32 @@ internal static class AdaptiveShoutCache
 
     internal static string GetLocalizedRandomLine(string[] key, bool? catBackup = null, params object[] args)
     {
-        var baseLine = ModEntry.Instance.Localizations.Localize(GetRandomLine(key, catBackup));
+        var line = GetRandomLine(key, catBackup);
+        if (line is null) return "<c=redd>[[Missing Line (THIS SHOULD NOT HAPPEN)]]</c>";
+        var baseLine = ModEntry.Instance.Localizations.Localize(line);
         return string.Format(baseLine, args);
     }
 
-    internal static string[] GetRandomLine(string[] key, bool? catBackup = null) =>
-        GetMostSpecificLines(key, catBackup).Random(rng);
+    internal static string[]? GetRandomLine(string[] key, bool? catBackup = null)
+    {
+        var lines = GetMostSpecificLines(key, catBackup);
+        if (lines.Count == 0) return null;
+        var lineKey = lines.Random(rng);
+        if (lineKey[^1].StartsWith("once")) lines.Remove(lineKey);
+        return lineKey;
+    }
 
-    internal static List<string[]> GetMostSpecificLines(string[] key, bool? catBackup = null)
+    internal static string KeyToMapping(string[] key, bool? catBackup)
     {
         var mappingKey = key.Aggregate((s1, s2) => $"{s1}/{s2}");
         if (catBackup is not null)
             mappingKey += catBackup.Value ? "/backup" : "/cat";
+        return mappingKey;
+    }
+
+    internal static List<string[]> GetMostSpecificLines(string[] key, bool? catBackup = null)
+    {
+        var mappingKey = KeyToMapping(key, catBackup);
         
         // If the lines exist in the cache we just return them
         if (_mappingCache.TryGetValue(mappingKey, out var lines))
@@ -37,32 +51,45 @@ internal static class AdaptiveShoutCache
         {
             // If the key match completely fails, we return an empty list
             _mappingCache[mappingKey] = [];
-            return [];
+            return _mappingCache[mappingKey];
         }
         test_subkey:
-        var subkeyFull = (
-            catBackup is null ? subkey
-            : catBackup.Value ? subkey.Append("backup")
-            : subkey.Append("cat")
-        ).ToList();
-        var firstLine = loc.Localize(subkeyFull.Append("1").ToArray());
-        if (firstLine is null)
-        {
-            subkey.RemoveAt(subkey.Count - 1);
-            goto test_subkey;
-        }
+            var subkeyFull = (
+                catBackup is null ? subkey
+                : catBackup.Value ? subkey.Append("backup")
+                : subkey.Append("cat")
+            ).ToList();
+            var firstLine = loc.Localize(subkeyFull.Append("1").ToArray());
+            if (firstLine is null)
+            {
+                subkey.RemoveAt(subkey.Count - 1);
+                goto test_subkey;
+            }
         
         // If we found a corresponding key, get all keys within that list
+        // Normal keys
         List<string[]> res = [];
         var i = 1;
         start_enum_key:
-        var enumKey = subkeyFull.Append(i.ToString()).ToArray();
-        if (loc.Localize(enumKey) is null) goto finish_enum_key;
-        res.Add(enumKey);
-        i++;
-        goto start_enum_key;
+            var enumKey = subkeyFull.Append(i.ToString()).ToArray();
+            if (loc.Localize(enumKey) is null) goto finish_enum_key;
+            res.Add(enumKey);
+            i++;
+            goto start_enum_key;
         finish_enum_key:
+        
+        // Once keys
+        i = 1;
+        start_enum_key_once:
+            enumKey = subkeyFull.Append($"once{i}").ToArray();
+            if (loc.Localize(enumKey) is null) goto finish_enum_key_once;
+            res.Add(enumKey);
+            i++;
+            goto start_enum_key_once;
+        finish_enum_key_once:
+        
+        // Save in cache and return
         _mappingCache[mappingKey] = res;
-        return res;
+        return _mappingCache[mappingKey];
     }
 }
